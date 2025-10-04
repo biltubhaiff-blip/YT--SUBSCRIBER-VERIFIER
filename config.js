d6bb5b433d463c5b7f5b6d42d3df981f4978fea0
biltubhaiff-blip <biltubhaiff@gmail.com>
2025-10-04 14:05:28 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: "MTQyMzk0MjY4NjM5Nzk2MDMyNQ.GO1TiJ.4ejc28q2fSjlFvnsPMwTmpXizaWwn96itTXYI4", // Add your bot's token here (REQUIRED)
+    channel_name: "@HydraBlade-Sub", // Specify your YouTube channel's name here (REQUIRED)
+    role_id: "1410535530717773894", // ID of the role you want to assign (OPTIONAL)
+    keywords: "subscribe, follow, like", // Specify the keywords separated by commas (OPTIONAL)
+    save_data: "true" // Save data in subscriber.json (OPTIONAL)
+};
